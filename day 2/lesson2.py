cb34827f1e1837f97c5cb01e18897a47bbff8844
nika1keshelava <nika.keshelava.1@btu.edu.ge>
2022-09-22 22:48:16 +0400

    day2 commit

--- day 2/lesson2.py
+++ day 2/lesson2.py
@@ -1,1 +1,54 @@
-print("this is a lesson 2")
+# name = "nika"
+
+# print(len(name))    #len  length
+#           #4
+
+# my_sentence = """გამარჯობა, შეგახსენებთ რომ სწავლას
+# გააცდინოთ"""
+
+# print(my_sentence)
+
+# knows_programming = True
+
+
+# if 10>5:
+#     print("hello")
+# tab-ის საშუალებით მოვახდინეთ ინდენტაცია (indentation)
+
+
+# full_name = "nika keshelava"
+# print(len(full_name))
+
+# ყველა სტრინგი შეგვიძლია მივიჩნიოთ სიად
+# print(full_name[7])
+# position = index სინონიმებია
+
+# print("k" not in full_name)
+
+full_name = "nika keshelava"
+# print(full_name[2:9])
+# print(full_name[2:9:2])
+# start:finish:step
+
+# print(full_name[:5])
+# print(full_name[5:])
+
+# print(full_name[-1:-6:-1])
+
+
+#სტრინგებს აქვთ მეთოდები
+#  1) სტრინგ.upper()
+#  2) len(სტრინგ)
+
+print(full_name.upper())
+
+name2 = "    NIKA   "
+# print(name2.lower())
+
+# print(name2.strip())
+
+
+print(name2.replace(" ", "#"))
+
+# "saba vakhtangadze"
+# "s$b$ v$"
