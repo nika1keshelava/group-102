dbc82e965b162dfd5cafbaa2e722b9ee83e32df6
nika1keshelava <nika.keshelava.1@btu.edu.ge>
2022-09-22 21:20:40 +0400

    day1 codes commit

---
+++ day 1/lesson1.py
@@ -0,0 +1,34 @@
+# name = "nika"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+# "nika" არის ცვლადისთვის მნიშვნელობა
+
+surname = "keshelava"
+
+# print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+name = "nika"  #ეს არის str (string) ტიპის ცვლადი
+age = 22 # ეს არის int ( integer ) მთელი რიცხვი 
+height = 184.5 #ეს არის float ტიპის ცვლადი ( ათწილადი )
+#boolean (bool) ტიპის ცვლადი
+
+knows_programming = True   #True ან False 
+is_ugly = False   #snakecase( უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+# print(name + " " + surname)
+
+# #სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+
+
+# print(type(age))
+# print(type(name))
+# print(type(surname))
+# print(type(height))
+# print(type(knows_programming))
+
+print(name + " " + str(age))
+
